f9db8d95113111c6697fdebaa5072365d0396563
Mike <miguel.maya.27@gmail.com>
2017-06-10 17:47:33 -0500

    Correccion de recursos compartidos

--- cliente.java
+++ cliente.java
@@ -10,12 +10,24 @@ import java.util.LinkedList;
 
 class cliente{
 
+
 	public static void main(String args[]) throws Exception{
+
+		Hashtable<String, Vector> Archivos= new Hashtable<String, Vector>();
+		Queue<String> Pendientes = new LinkedList<String>();
+		Vector IPS = new Vector();
+
 		DatagramSocket socketServicio=new DatagramSocket(7744);	
 		servicio detectarServicios = new servicio(1);
 		servicio broadcast= new servicio(2);
 		servicio escuchar= new servicio(3);
 		servicio manejoDirectorios= new servicio(4);
+
+		detectarServicios.setEstructuras(Archivos, Pendientes, IPS);
+		broadcast.setEstructuras(Archivos, Pendientes, IPS);
+		escuchar.setEstructuras(Archivos, Pendientes, IPS);
+		manejoDirectorios.setEstructuras(Archivos, Pendientes, IPS);
+
 		//Seteamos el socket compartido
 		detectarServicios.setSocket(socketServicio);
 		broadcast.setSocket(socketServicio);
